b90ea54e78fb9dfd5bdc1c7333b97040158cf31b
ngoc10 <ngohongngoc2003aa@gmail.com>
2024-05-12 12:03:20 +0700

    TPham

---
+++ NETNENNET/THUCPHAM.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace NETNENNET
+{
+    partial class THUCPHAM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDong = new System.Windows.Forms.Button();
+            this.btnBoqua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.DatagridviewDMthucpham = new System.Windows.Forms.DataGridView();
+            this.cboMaDMSP = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTenDMTP = new System.Windows.Forms.TextBox();
+            this.txtMaDMTP = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoluong = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DatagridviewDMthucpham)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(1009, 535);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(89, 37);
+            this.btnDong.TabIndex = 84;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            // 
+            // btnBoqua
+            // 
+            this.btnBoqua.Location = new System.Drawing.Point(823, 535);
+            this.btnBoqua.Name = "btnBoqua";
+            this.btnBoqua.Size = new System.Drawing.Size(89, 37);
+            this.btnBoqua.TabIndex = 83;
+            this.btnBoqua.Text = "Bỏ qua";
+            this.btnBoqua.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(602, 535);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(89, 37);
+            this.btnLuu.TabIndex = 82;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(404, 535);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(89, 37);
+            this.btnXoa.TabIndex = 81;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(221, 535);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(89, 37);
+            this.btnSua.TabIndex = 80;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(40, 535);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(89, 37);
+            this.btnThem.TabIndex = 79;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // DatagridviewDMthucpham
+            // 
+            this.DatagridviewDMthucpham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DatagridviewDMthucpham.Location = new System.Drawing.Point(41, 236);
+            this.DatagridviewDMthucpham.Name = "DatagridviewDMthucpham";
+            this.DatagridviewDMthucpham.RowHeadersWidth = 51;
+            this.DatagridviewDMthucpham.RowTemplate.Height = 24;
+            this.DatagridviewDMthucpham.Size = new System.Drawing.Size(1068, 278);
+            this.DatagridviewDMthucpham.TabIndex = 78;
+            // 
+            // cboMaDMSP
+            // 
+            this.cboMaDMSP.AutoSize = true;
+            this.cboMaDMSP.Location = new System.Drawing.Point(627, 109);
+            this.cboMaDMSP.Name = "cboMaDMSP";
+            this.cboMaDMSP.Size = new System.Drawing.Size(71, 16);
+            this.cboMaDMSP.TabIndex = 76;
+            this.cboMaDMSP.Text = "Mã DMTP:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(448, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 38);
+            this.label1.TabIndex = 75;
+            this.label1.Text = "THỰC PHẨM\r\n";
+            // 
+            // txtTenDMTP
+            // 
+            this.txtTenDMTP.Location = new System.Drawing.Point(209, 165);
+            this.txtTenDMTP.Name = "txtTenDMTP";
+            this.txtTenDMTP.Size = new System.Drawing.Size(294, 22);
+            this.txtTenDMTP.TabIndex = 74;
+            // 
+            // txtMaDMTP
+            // 
+            this.txtMaDMTP.Location = new System.Drawing.Point(209, 109);
+            this.txtMaDMTP.Name = "txtMaDMTP";
+            this.txtMaDMTP.Size = new System.Drawing.Size(294, 22);
+            this.txtMaDMTP.TabIndex = 73;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(105, 165);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 16);
+            this.label8.TabIndex = 72;
+            this.label8.Text = "Tên Thực phẩm:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(105, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 16);
+            this.label9.TabIndex = 71;
+            this.label9.Text = "Mã Thực phẩm:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 85;
+            this.label3.Text = "Số lượng:";
+            // 
+            // txtSoluong
+            // 
+            this.txtSoluong.Location = new System.Drawing.Point(704, 168);
+            this.txtSoluong.Name = "txtSoluong";
+            this.txtSoluong.Size = new System.Drawing.Size(294, 22);
+            this.txtSoluong.TabIndex = 86;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(704, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(294, 24);
+            this.comboBox1.TabIndex = 87;
+            // 
+            // THUCPHAM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 592);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtSoluong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnBoqua);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.DatagridviewDMthucpham);
+            this.Controls.Add(this.cboMaDMSP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTenDMTP);
+            this.Controls.Add(this.txtMaDMTP);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Name = "THUCPHAM";
+            this.Text = "THUCPHAM";
+            ((System.ComponentModel.ISupportInitialize)(this.DatagridviewDMthucpham)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Button btnBoqua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.DataGridView DatagridviewDMthucpham;
+        private System.Windows.Forms.Label cboMaDMSP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTenDMTP;
+        private System.Windows.Forms.TextBox txtMaDMTP;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoluong;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
